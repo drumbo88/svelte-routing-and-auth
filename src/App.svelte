da72ba677b2418fb982894cdcd1461cd59b16d2a
Darío Rumbo <dario.rumbo@gmail.com>
2023-04-26 14:27:24 -0300

    Initial basic routing

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+    import { Route, Link, Router } from 'svelte-routing'
+  import Home from './views/Home.svelte';
+  import About from './views/About.svelte';
+  import Profile from './views/Profile.svelte';
+</script>
+
+<div>
+    <h1>Routing</h1>
+    <Router>
+        <nav>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/profile">Profile</Link>
+        </nav>
+        <Route path="/">
+            <Home />
+        </Route>
+        <Route path="/about">
+            <About />
+        </Route>
+        <Route path="/profile">
+            <Profile />
+        </Route>
+     </Router>
+</div>
---
+++ src/views/Profile.svelte
@@ -0,0 +1,5 @@
+<script>
+
+</script>
+
+<h2>Profile (protected)</h2>
